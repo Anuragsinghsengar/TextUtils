f1101286be54066e11fbe040022068f6b4aaaf0b
Anuragsinghsengar <131814714+Anuragsinghsengar@users.noreply.github.com>
2023-11-08 20:42:31 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
       setMode('dark')
       document.body.style.backgroundColor = '#665a42'
       showAlert('Dark Mode has been enabled','success');
-      document.title = 'TextUtils2- A wonderful text analyzer[Dark Mode]'
+      document.title = 'TextUtils- A wonderful text analyzer[Dark Mode]'
       // setCurrentMode('Enable Dark Mode');
     }
   }
@@ -50,7 +50,7 @@ function App() {
       setMode('blue')
       document.body.style.backgroundColor = '#042743'
       showAlert('Blue Mode has been enabled','success');
-      document.title = 'TextUtils2- A wonderful text analyzer[Blue Mode]'
+      document.title = 'TextUtils- A wonderful text analyzer[Blue Mode]'
       // setCurrentMode('Enable Dark Mode');
     }
   }
@@ -64,7 +64,7 @@ function App() {
       setMode('light')
       document.body.style.backgroundColor = '#EDEADE'
       showAlert('Light Mode has been enabled','success');
-      document.title = 'TextUtils2- A wonderful text analyzer[Light Mode]'
+      document.title = 'TextUtils- A wonderful text analyzer[Light Mode]'
       // setCurrentMode('Enable Dark Mode');
     }
   }
@@ -78,7 +78,7 @@ function App() {
       setMode('green')
       document.body.style.backgroundColor = '#19543E'
       showAlert('Green Mode has been enabled','success');
-      document.title = 'TextUtils2- A wonderful text analyzer[Green Mode]'
+      document.title = 'TextUtils- A wonderful text analyzer[Green Mode]'
       // setCurrentMode('Enable Dark Mode');
     }
   }
@@ -92,7 +92,7 @@ function App() {
       setMode('red')
       document.body.style.backgroundColor = '#8B0000'
       showAlert('Red Mode has been enabled','success');
-      document.title = 'TextUtils2- A wonderful text analyzer[Red Mode]'
+      document.title = 'TextUtils- A wonderful text analyzer[Red Mode]'
       // setCurrentMode('Enable Dark Mode');
     }
   }
@@ -106,7 +106,7 @@ function App() {
       setMode('yellow')
       document.body.style.backgroundColor = '#DE970B'
       showAlert('Yellow Mode has been enabled','success');
-      document.title = 'TextUtils2- A wonderful text analyzer[Yellow Mode]'
+      document.title = 'TextUtils- A wonderful text analyzer[Yellow Mode]'
       // setCurrentMode('Enable Dark Mode');
     }
   }
